3eb3db67ae3189c40cd8f3ac5ffb59a6089cc13a
RFPadilha <34008458+RFPadilha@users.noreply.github.com>
2021-09-13 22:39:09 -0300

    Generates Map on Click
    -procedurally generates maps via click
    -needs to validate playable maps
    -needs better generation

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    ProceduralGeneration PCG;
+
+    public List<Transform> targets;
+    public Vector3 offset;
+
+    void Start()
+    {
+        PCG = FindObjectOfType<ProceduralGeneration>();
+
+    }
+    void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            targets = GetTargets();
+            if (targets.Count == 0)
+                return;
+            Vector3 centerPoint = GetCenterPoint();
+            Vector3 newPosition = centerPoint + offset;
+            transform.position = newPosition;
+        }
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i=0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+
+    List<Transform> GetTargets()
+    {
+        List<Transform> targetsToEncapsulate = new List<Transform>();
+        foreach (Transform tile in PCG.transform)
+        {
+            targetsToEncapsulate.Add(tile);
+        }
+
+        return targetsToEncapsulate;
+    }
+}
+
+
---
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+    [SerializeField] int mapWidthMultiplier;
+    [SerializeField] int repeatNum;
+    [SerializeField] GameObject ground;//referencia ao prefab do tile do chão
+    Vector2 direction;
+    public int width, height;
+
+    public GameObject startingPos;
+    void Start()
+    {
+        //Generate();
+        Cursor.lockState = CursorLockMode.Locked;//usado para centralizar o mouse na tela no início
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))//quando pressionado o botão esquerdo
+        {
+            DestroyPreviousMap();
+
+            Vector2 screenPos = new Vector2(mousePos.x, mousePos.y);
+            direction = new Vector2(screenPos.x - startingPos.transform.position.x, screenPos.y - startingPos.transform.position.y);
+            Generate(direction);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Generate(Vector2 direction)
+    {
+        width = Mathf.Abs(Mathf.FloorToInt(direction.x)*mapWidthMultiplier);
+        height = Mathf.Abs(Mathf.FloorToInt(direction.y));
+
+        int minHeight = 1;
+        int maxHeight = height;
+
+        int repeatValue = 0;    
+
+        for (int i = -width/2; i < width/2; i++)
+        {
+            if (repeatValue == 0)
+            {
+                height = Random.Range(minHeight, maxHeight);
+                GeneratePlatform(i, height);
+                repeatValue = repeatNum;
+            }
+            else
+            {
+                GeneratePlatform(i, height);
+                repeatValue--;
+            }
+        }
+    }
+
+    void GeneratePlatform(int x, int y)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            SpawnObject(ground, x, j);
+        }
+    }
+
+    void SpawnObject(GameObject obj, int width, int height)//o que for criado estará como folha do objeto que contém o script
+    {
+        obj = Instantiate(obj, new Vector2(width, height), Quaternion.identity);
+        obj.transform.parent = this.transform;
+    }
+
+    void DestroyPreviousMap()
+    {
+        foreach(Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
