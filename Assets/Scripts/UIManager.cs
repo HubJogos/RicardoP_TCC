c2be26ac3db677355978f40cc25a38c9bf906053
RFPadilha <34008458+RFPadilha@users.noreply.github.com>
2022-03-29 16:29:12 -0300

    Fixed Object Placement
    -Attempted Pathfinding

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public PlayerScript playerScript;//references to use healthbar
+    public Slider healthBar;
+    public Text hpText;
+    public Slider ammoCounter;
+    void Start()
+    {
+        playerScript = FindObjectOfType<PlayerScript>();
+    }
+
+    void Update()
+    {
+        ammoCounter.maxValue = playerScript.maxAmmo;
+        ammoCounter.value = playerScript.currentAmmo;
+        healthBar.maxValue = playerScript.maxHealth;
+        healthBar.value = playerScript.currentHealth;//controls updating of healthbar
+        hpText.text = "HP: " + playerScript.maxHealth + "/" + playerScript.currentHealth;
+    }
+}
